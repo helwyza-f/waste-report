75e7e2b90869333e1cbe3775b3ba6aef8fe5ece7
Helwiza Fahry <helwyza@gmail.com>
2025-07-25 16:58:33 +0700

    add richColors to toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
         >
           <Navbar />
           <main className="pt-5 px-4 max-w-6xl mx-auto">{children}</main>
-          <Toaster />
+          <Toaster richColors />
           {/* <PushPermissionBanner />  */}
 
           <PWAInstallPrompt />
